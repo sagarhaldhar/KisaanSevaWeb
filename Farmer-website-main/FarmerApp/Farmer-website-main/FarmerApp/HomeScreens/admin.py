52db96b2692303d895cd7186b37249df166df9d4
Sagar Haldhar <sagarhaldhar76@gmail.com>
2024-06-02 22:25:39 +0530

    KB laga diye

--- Farmer-website-main/FarmerApp/Farmer-website-main/FarmerApp/HomeScreens/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Testimonial,Feedback
-# Register your models here.
-admin.site.register(Testimonial)
-admin.site.register(Feedback)
-# admin.py
-
